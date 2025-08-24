8cc34e5fbc7eae9466fba1827a24aa5a959f59db
afrze <afroze@outlook.in>
2025-08-24 07:00:24 +0000

    feat(#1): :sparkles: Added tailwind to web

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	server: { port: 3003 },
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), tailwindcss()]
 });
